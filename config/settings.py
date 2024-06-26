b7ba286c12d2c7c9ab12229a96f3e69f00eccfbc
Muhammadqodir <155760560+Muhammadqodir17@users.noreply.github.com>
2024-06-26 19:56:21 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -73,13 +73,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "muhammadqodir_social-media",
-        "USER": "doadmin",
-        "PASSWORD": "AVNS_feJOFOu7KkwbdfMc3JX",
-        "HOST": "do-db-do-user-16947834-0.c.db.ondigitalocean.com",
-        "PORT": "25060",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
